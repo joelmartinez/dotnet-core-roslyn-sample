4ca501296f4a63aa322de002350292871e25635b
Joel Martinez <joelmartinez@gmail.com>
2016-06-02 16:28:10 -0400

    project code

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        static Action<string> Write = Console.WriteLine;
+        
+        public static void Main(string[] args)
+        {
+            Write("Let's compile!");
+            
+            string codeToCompile = @"
+            using System;
+
+            namespace RoslynCompileSample
+            {
+                public class Writer
+                {
+                    public void Write(string message)
+                    {
+                        Console.WriteLine($""you said '{message}!'"");
+                    }
+                }
+            }";
+            
+            Write("Parsing the code into the SyntaxTree");
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(codeToCompile);
+            
+            string assemblyName = Path.GetRandomFileName();
+            MetadataReference[] references = new MetadataReference[]
+            {
+                MetadataReference.CreateFromFile(typeof(object).GetTypeInfo().Assembly.Location)
+            };
+
+            Write("Compiling ...");
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                assemblyName,
+                syntaxTrees: new[] { syntaxTree },
+                references: references,
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+    
+            using (var ms = new MemoryStream())
+            {
+                EmitResult result = compilation.Emit(ms);
+
+                if (!result.Success)
+                {
+                    Write("Compilation failed!");
+                    IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic => 
+                        diagnostic.IsWarningAsError || 
+                        diagnostic.Severity == DiagnosticSeverity.Error);
+
+                    foreach (Diagnostic diagnostic in failures)
+                    {
+                        Console.Error.WriteLine("\t{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
+                    }
+                }
+                else
+                {
+                    Write("Compilation successful! Now instantiating and executing the code ...");
+                    ms.Seek(0, SeekOrigin.Begin);
+                    
+                    Assembly assembly = AssemblyLoadContext.Default.LoadFromStream(ms);
+                    var type= assembly.GetType("RoslynCompileSample.Writer");
+                    var instance = assembly.CreateInstance("RoslynCompileSample.Writer");
+                    var meth = type.GetMember("Write").First() as MethodInfo;
+                    meth.Invoke(instance, new [] {"joel"});
+                }
+            }
+    
+    
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+Using Roslyn in .NET Core
+-------------------------
+This is a simple example of using Roslyn to compile some code from a string,
+ load it into the currently executing `AppDomain`, and activate and invoke the code.
+ It is based on [this article](http://www.tugberkugurlu.com/archive/compiling-c-sharp-code-into-memory-and-executing-it-with-roslyn)
+ and was adapted for the slightly different API surface area on .net core.
+ 
+## Dependencies
+This was written to work with [.NET Core RC2](https://github.com/dotnet/core/releases/tag/v1.0.0-rc2). 
+In order to resolve the prerelease dependencies, you'll have to add the `aspnetvidev` myget feed. 
+You can do so by adding or modifying the
+[`~/.config/NuGet/NuGet.Config`](https://docs.nuget.org/consume/nuget-config-file) 
+file (on mac) to include the `AspNetCiDev` key listed below:
+
+```xml
+<?xml version="1.0" encoding="utf-8"?>
+<configuration>
+  <activePackageSource>
+    <add key="AspNetCiDev" value="https://www.myget.org/F/aspnetcidev/api/v3/index.json" />
+    <add key="Official NuGet Gallery" value="https://www.nuget.org/api/v2/" />
+  </activePackageSource>
+</configuration>
+```
+
+## Running the Sample
+
+```bash
+git clone https://github.com/joelmartinez/dotnet-core-roslyn-sample.git
+cd dotnet-core-roslyn-sample
+dotnet restore && dotnet run
+```
